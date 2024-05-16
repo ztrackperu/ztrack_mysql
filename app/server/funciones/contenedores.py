c3141df86f9bb2999d11d5817abb74bbec380786
ztrackperu <ztrack@zgroup.com.pe>
2024-05-16 14:51:38 -0500

    con extracion de data9

--- app/server/funciones/contenedores.py
+++ app/server/funciones/contenedores.py
@@ -79,9 +79,9 @@ async def validar_live(notificacion_data: dict) -> dict:
     actualizar =[]
     for val in pro:
         #validaciond de datos internos 
-        print(val['telemetria_id']) 
+        #print(val['telemetria_id']) 
         ultimaFech =val['ultima_fecha'].replace('T',' ')
-        print(ultimaFech)
+        #print(ultimaFech)
         query = "SELECT telemetria_id,ultima_fecha,descripcionC,set_point,latitud,longitud,temp_supply_1,return_air,evaporation_coil,condensation_coil"
         query +=",compress_coil_1,ambient_air,cargo_1_temp,cargo_2_temp,cargo_3_temp,cargo_4_temp,relative_humidity,avl,line_voltage,line_frequency"
         query +=",consumption_ph_1,consumption_ph_2,consumption_ph_3,co2_reading,power_kwh,power_state,humidity_set_point,controlling_mode,set_point_co2"
@@ -92,10 +92,10 @@ async def validar_live(notificacion_data: dict) -> dict:
         dataz = cursor.fetchone()
         conn.commit()
         cursor.close()
-        print(dataz)
-        print(str(dataz['ultima_fecha']))
+        #print(dataz)
+        #print(str(dataz['ultima_fecha']))
         if(str(dataz['ultima_fecha'])!=ultimaFech):
-            print("debemos actualizar los datos")
+            #print("debemos actualizar los datos")
             actualizar.append(dataz)
     #print(pro[0])
     print(actualizar)
